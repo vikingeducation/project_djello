f14b2dc682a7b927c44273bc4892aa950ca08f7c
Garrett <garrettweinert@gmail.com>
2015-09-22 09:28:18 -0700

    working with devise

--- config/application.rb
+++ config/application.rb
@@ -21,6 +21,11 @@ module AssignmentDjello
     # config.i18n.default_locale = :de
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
+
+    # DEVISE SETUP FOR HEROKU DEPLOY
+    config.assets.initialize_on_precompile = false
+    
+
     config.active_record.raise_in_transactional_callbacks = true
   end
 end
