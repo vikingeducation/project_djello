ca287582d19435a4070a609e0cbfeedd5f2d68db
mikebutts <mgbutts@gmail.com>
2015-09-22 13:25:08 -0400

    building board

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,10 @@ Rails.application.routes.draw do
   devise_for :users, :controllers => {sessions: 'sessions'}
 
   root to: "main#index"
+  
+  scope :api do
+    scope :v1 do
+      resources :boards, :except => [:new, :edit]
+    end
+  end
 end
