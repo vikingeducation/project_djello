31604b0c491e01c21394d000ec96b798126b0614
Alice Fung <alice.o.fung@gmail.com>
2015-09-22 19:26:00 -0400

    before merge

--- djello/db/schema.rb
+++ djello/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150922184511) do
+ActiveRecord::Schema.define(version: 20150922184503) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,14 +21,6 @@ ActiveRecord::Schema.define(version: 20150922184511) do
     t.integer  "user_id",    null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-  end
-
-  create_table "cards", force: :cascade do |t|
-    t.string   "title",       null: false
-    t.string   "description", null: false
-    t.integer  "user_id",     null: false
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
   end
 
   create_table "lists", force: :cascade do |t|
