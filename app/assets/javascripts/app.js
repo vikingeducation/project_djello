889e73fe516a314f3bd214582da7c6397d6eeb62
Garrett <garrettweinert@gmail.com>
2015-09-22 08:40:07 -0700

    set up rails

---
+++ app/assets/javascripts/app.js
@@ -0,0 +1,18 @@
+djelloApp = angular.module('djelloApp', ['ui.router', 'restangular', 'Devise']);
+
+djelloApp..config(['RestangularProvider', function(RestangularProvider){
+
+   RestangularProvider.setBaseUrl('/api/v1');
+    RestangularProvider.setRequestSuffix('.json');
+    RestangularProvider.setDefaultHttpFields({
+        "content-type": "application/json"
+    });
+
+}]);
+
+djelloApp.config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider){
+
+  $stateProvider
+
+  .state();
+});
---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,26 @@
+// This is a manifest file that'll be compiled into application.js, which will include all the files
+// listed below.
+//
+// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
+// or any plugin's vendor/assets/javascripts directory can be referenced here using a relative path.
+//
+// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
+// compiled file.
+//
+// Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
+// about supported directives.
+//
+//= require jquery
+//= require jquery_ujs
+
+//= require angular
+
+// AFTER angular, BEFORE require_tree:
+//= require angular-ui-router.min.js
+//= require lodash.min.js
+//= require restangular.min.js
+//= require underscore.min.js
+
+//= require angular-devise/lib/devise
+
+//= require_tree .
