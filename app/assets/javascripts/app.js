d0f1e5da21238d57dc593e428e5e247f3e5c01be
Joseph Lozano <lozanojoseph@gmail.com>
2015-09-22 08:44:39 -0700

    HELLO WORLD

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -1,1 +1,21 @@
-var djello = angular.module("djello", ['ui.router', 'Devise', 'Restangular'])
+var djello = angular.module("djello", ['ui.router', 'Devise', 'restangular'])
+
+.config(['RestangularProvider', function(RestangularProvider){
+
+RestangularProvider.setBaseUrl('/api/v1/')
+RestangularProvider.setRequestSuffix('.json')
+
+}])
+
+.config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider){
+
+  $urlRouterProvider.otherwise("/")
+
+  $stateProvider
+    .state('index', {
+      url: "/",
+      templateUrl: 'templates/index.html',
+      controller: function(){}
+    })
+
+}])
