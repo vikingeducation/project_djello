a30be8025fda2af44d56fc4931ffba61e4fee0f6
alokp <alokpra@gmail.com>
2015-09-23 17:59:43 -0700

    working on card creation

--- app/assets/javascripts/controllers/boardShowCtrl.js
+++ app/assets/javascripts/controllers/boardShowCtrl.js
@@ -4,9 +4,13 @@ djelloApp.controller('boardShowCtrl',
     // board;
     $scope.board = board;
     $scope.newList = {};
+    $scope.addNewList = false;
 
     $scope.addList = function(){
+      $scope.addNewList = true;
+    };
 
+    $scope.createList = function(){
       Restangular.all('lists').post(
         { list: {
                   board_id: $scope.board.id,
@@ -16,6 +20,7 @@ djelloApp.controller('boardShowCtrl',
         }).then(function(createdList){
           console.log(board);
           $scope.board.lists.push(createdList);
+          $scope.addNewList = false;
       });
   };
 
--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,16 +1,29 @@
 class ListsController < ApplicationController
 
   def create
-    @list = List.new(whitelist_list_params)
-    # binding.pry
-    @list.user_id = current_user.id
+    @list = List.create(whitelist_list_params)
+
+    respond_to do |format|
+      if @list.save
+        format.json {render json: @list}
+      else
+        format.json {render status: :unprocessable_entity}
+      end
+    end
 
   end
 
   def destroy
     @list = List.find(params['id'])
 
-    
+    respond_to do |format|
+      if @list.destroy
+        @lists = List.all
+        format.json { render json: @lists }
+      else
+        format.json { render status: :unprocessable_entity }
+      end
+    end
   end
 
   private
