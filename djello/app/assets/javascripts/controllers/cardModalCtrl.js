964cf2dc810d74b5602dc3e09b83dd6336523a5a
Alice Fung <alice.o.fung@gmail.com>
2015-09-24 13:42:59 -0400

    refactored edit card method

--- djello/app/assets/javascripts/controllers/cardModalCtrl.js
+++ djello/app/assets/javascripts/controllers/cardModalCtrl.js
@@ -20,47 +20,49 @@ djello.controller('cardModalCtrl', [
   };
 
   //=================card editing methods==================
+
+  // to populate editable data in modal
   var setEditData = function(){
     $scope.oldCard = { id: card.id,
                     title: card.title,
               description: card.description};
   };
 
+  setEditData(); // running fn when modal opens
+
   var getRectangularObj = function(){
     return Restangular.one('lists', card.list_id)
                                     .one('cards', card.id);
   };
 
-  setEditData();
   $scope.editCard = function(input){
-    if (input == 'cancel' && $scope.editCardEnabled) {
-      $scope.oldCard =  $scope.card;
+    if (input == 'saved' && $scope.editCardEnabled){
+      updateCard();
+    } else {
+      setEditData();
     }
-    else if (input == 'saved' && $scope.editCardEnabled){
-      var updatedCard = getRectangularObj();
-
-      updatedCard.title = $scope.oldCard.title;
-      updatedCard.description = $scope.oldCard.description;
-
-      updatedCard.put().then( function(){
-        $scope.card.title = updatedCard.title;
-        $scope.card.description= updatedCard.description;
-        $scope.oldCard = $scope.card; // why is this line necessary?
-      } );
-    }
-
-    setEditData();
-
     $scope.editCardEnabled  = !$scope.editCardEnabled;
+  };
+
+  var updateCard = function(){
+    var updatingCard = getRectangularObj();
+    // set restangular obj properties with updated data
+    updatingCard.title = $scope.oldCard.title;
+    updatingCard.description = $scope.oldCard.description;
+
+    updatingCard.put().then( function(result){
+      $scope.card.title = result.title;
+      $scope.card.description= result.description;
+    } );
   };
 
   $scope.cardComplete = function(){
     getRectangularObj().remove().then(function(){
       $scope.list.cards.splice(idxInList, 1);
+      $scope.close();
     }
       );
   };
-
 
 
 }]);
