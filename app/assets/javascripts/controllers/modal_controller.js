5628d65b767c522d2aff6852f2a8ae1e94348849
Julia Herron <julia.herron@gmail.com>
2016-04-05 10:13:50 -0700

    youuuuuuuuuuuu sneaky scope

--- app/assets/javascripts/controllers/modal_controller.js
+++ app/assets/javascripts/controllers/modal_controller.js
@@ -1,18 +1,52 @@
-djello.controller('ModalCtrl', ['$scope', 'ModalService', function($scope, ModalService) {
+djello.controller('ModalCtrl', ['$scope', 'ModalService', 'CardsService', 'ListsService', function($scope, ModalService, CardsService, ListsService) {
 
 
-  $scope.show = function() {
-    console.log("got into show?")
+  $scope.allLists = ListsService.currentLists;
+  // $scope.currentList = {};
+  // $scope.currentCard = {};
+  // $scope.title = "HI"
+
+
+  $scope.show = function(event) {
+
+    var listId = Number($(event.target).attr("list-id"));
+    var cardId = Number($(event.target).attr("card-id"))
+    // console.log("got into show?")
+    // console.log($(event.target).attr("list-id"))
+    // console.log(typeof $(event.target).attr("list-id"))
+    // console.log($(event.target).attr("card-id"))
+    // console.log($scope.allLists)
+    // console.log(_.filter($scope.allLists, {id: "215"}))
+    console.log(listId)
+    console.log(cardId)
+
+    $scope.currentList = CardsService.getCurrentList($scope.allLists, listId);
+    // $scope.allCards = $scope.currentList.cards;
+    $scope.currentCard = CardsService.getCurrentCard($scope.currentList.cards, cardId)
+
+    console.log($scope.currentCard)
+
     ModalService.showModal({
       templateUrl: 'templates/card_modal.html',
-      controller: "ModalController"
+      scope: $scope, // This was the key!!!!
+      controller: "ModalController" //,
+      // resolve: {
+      //   list: function() {
+      //     return scope.currentList;
+      //   },
+      //   card: function() {
+      //     return scope.currentCard;
+      //   }
+      // }
     }).then(function(modal) {
       modal.element.modal();
       modal.close.then(function(result) {
         console.log(result)
       })
     })
-  }
+  };
+
+
 
 }])
 
--- app/assets/javascripts/services/cards_service.js
+++ app/assets/javascripts/services/cards_service.js
@@ -1,11 +1,18 @@
-djello.factory('CardsCtrl', ['Restangular', function(Restangular) {
+djello.factory('CardsService', ['Restangular', function(Restangular) {
 
-  var obj;
-
-  obj.currentCards = [];
+  var obj = {};
 
 
-  
+  obj.getCurrentList = function(allLists, listId) {
+    return _.filter(allLists, {id: listId})[0];
+  }
+
+
+  obj.getCurrentCard = function(allCards, cardId) {
+    return _.filter(allCards, {id: cardId})[0];
+  }
+
+
 
 
   return obj;
