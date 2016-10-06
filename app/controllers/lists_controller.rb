239d509588c72bd7bc0dbcc8357f8da020e86129
cjvirtucio87 <cjv287@gmail.com>
2016-10-06 16:36:49 -0700

    generated lists controller.

---
+++ app/controllers/lists_controller.rb
@@ -0,0 +1,37 @@
+class ListsController < ApplicationController
+  def create
+    @list = List.new(list_params)
+    if @list.save
+      respond_to do |format|
+        format.json { render json: @list, status: 200 }
+      end
+    end
+  end
+
+  def index
+    # We're only interested in a specific board's set of lists.
+    board = Board.find_by_id(params[:board_id])
+    @lists = board.lists
+    respond_to do |format|
+      format.json { render json: @lists, status: 200 }
+    end
+  end
+
+  def update
+  end
+
+  def destroy
+    @list = List.find_by_id(params[:id])
+    if @list.destroy
+      respond_to do |format|
+        format.json { head :no_content }
+      end
+    end
+  end
+
+  private
+
+    def list_params
+      params.require(:list).permit(:title,:description,:board_id)
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  get 'lists/create'
+
+  get 'lists/index'
+
+  get 'lists/update'
+
+  get 'lists/destroy'
+
   devise_for :users
 
   root to: "static_pages#index"
