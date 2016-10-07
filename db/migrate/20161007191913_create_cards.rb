7895f5edbb6732fd89fbe0da53eade73e54ec59e
cjvirtucio87 <cjv287@gmail.com>
2016-10-07 12:20:03 -0700

    generated Card model

---
+++ db/migrate/20161007191913_create_cards.rb
@@ -0,0 +1,11 @@
+class CreateCards < ActiveRecord::Migration[5.0]
+  def change
+    create_table :cards do |t|
+      t.string :title
+      t.text :body
+      t.boolean :completed
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161006231929) do
+ActiveRecord::Schema.define(version: 20161007191913) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,14 @@ ActiveRecord::Schema.define(version: 20161006231929) do
     t.datetime "updated_at",  null: false
     t.integer  "user_id"
     t.index ["user_id"], name: "index_boards_on_user_id", using: :btree
+  end
+
+  create_table "cards", force: :cascade do |t|
+    t.string   "title"
+    t.text     "body"
+    t.boolean  "completed"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "lists", force: :cascade do |t|
