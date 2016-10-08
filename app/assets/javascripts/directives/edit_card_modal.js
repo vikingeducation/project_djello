346fa0c532f7053f00ba99040c5b1e9f32e78f1a
cjvirtucio87 <cjv287@gmail.com>
2016-10-08 14:06:13 -0700

    Mark completed working.

--- app/assets/javascripts/directives/edit_card_modal.js
+++ app/assets/javascripts/directives/edit_card_modal.js
@@ -1,10 +1,10 @@
 app.directive('editCardModal',
-['CardService', function(CardService) {
+['CardService', '$timeout', function(CardService, $timeout) {
 
   return {
     restrict: 'E',
     templateUrl: 'templates/directives/edit_card_modal.html',
-    link: function (scope) {
+    link: function (scope, element) {
       // Have to separate form data from model so as not to have two-way
       // binding.
       scope.cardForm = {
@@ -19,9 +19,20 @@ app.directive('editCardModal',
         CardService.update(scope.cardForm);
         scope.bodyEditState = false;
       };
-      scope.markCompleted = function () {
+      scope.setCompleted = function() {
         scope.cardForm.completed = true;
-        scope.submitEditForm();
+        return scope.cardForm;
+      };
+      scope.delayedSetCompleted = function() {
+        return $timeout(scope.setCompleted, 700);
+      };
+      scope.markCompleted = function () {
+        element.children('#myModal').modal('hide');
+        return Promise.try(scope.delayedSetCompleted)
+          .then(scope.submitEditForm)
+          .catch(function(reason) {
+            console.log(reason);
+          });
       };
     }
   };
