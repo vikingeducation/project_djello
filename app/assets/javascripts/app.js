36031c83bb1e656584b5e857ee284f0b31bee000
Leo Ahnn <leo.ahnn@gmail.com>
2016-10-10 12:49:24 -0400

    alter app js injection

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -72,10 +72,10 @@ Djello.config([
 
 ])
 
-Djello.run(function(editableOptions, editableThemes) {
+Djello.run(['editableOptions', 'editableThemes', function(editableOptions, editableThemes) {
   editableThemes.bs3.buttonsClass = 'btn-sm';
   editableOptions.theme = 'bs3'; // bootstrap3 theme. Can be also 'bs2', 'default'
-});
+}]);
 
 Djello.run(['$rootScope', '$state', '$stateParams', function ($rootScope, $state, $stateParams) {
         $rootScope.$state = $state;
