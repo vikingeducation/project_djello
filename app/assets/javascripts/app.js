bf57ee4457f598fdcb5b7cced5c951c5bf87f392
“Matthew <matthew.hinea@gmail.com>
2016-10-10 19:50:06 -0700

    looking for bug

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -2,7 +2,7 @@ var app = angular.module("djello", ['ui.router', 'restangular', 'angularModalSer
 
 app.config(["$urlRouterProvider", "$stateProvider", function($urlRouterProvider, $stateProvider) {
 
-  $urlRouterProvider.otherwise("/boards")
+  $urlRouterProvider.otherwise("/boards");
 
   $stateProvider
 
