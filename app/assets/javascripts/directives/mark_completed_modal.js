435d17c103e0c60e66e5ce42d1a2ad63f20f0af2
cjvirtucio87 <cjv287@gmail.com>
2016-10-14 20:24:56 -0700

    Mark completed directive needs fix.

--- app/assets/javascripts/directives/mark_completed_modal.js
+++ app/assets/javascripts/directives/mark_completed_modal.js
@@ -2,11 +2,14 @@ app.directive('markCompletedModal',
 ['$timeout', function ($timeout) {
   return {
     restrict: 'E',
+    require: '^editCardModal',
     templateUrl: 'templates/directives/mark_completed_modal.html',
-    link: function(scope, element) {
+    link: function(scope, element, attrs, ctrl) {
+
+      // Need to access parent directive's controller somehow.
       scope.setCompleted = function() {
-        scope.cardForm.completed = true;
-        return scope.cardForm;
+        ctrl.cardForm.completed = true;
+        return ctrl.cardForm;
       };
       scope.delayedSetCompleted = function() {
         return $timeout(scope.setCompleted, 700);
