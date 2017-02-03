21fc13ed70fa4608ceaee6d83190cc5fdd841d4e
Deepak <grc.deepak@gmail.com>
2017-02-02 20:07:51 -0800

    epic 1 - user login with devise done

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -12,6 +12,8 @@ Rails.application.configure do
   # Show full error reports.
   config.consider_all_requests_local = true
 
+
+
   # Enable/disable caching. By default caching is disabled.
   if Rails.root.join('tmp/caching-dev.txt').exist?
     config.action_controller.perform_caching = true
@@ -25,6 +27,7 @@ Rails.application.configure do
 
     config.cache_store = :null_store
   end
+
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
@@ -51,4 +54,7 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+  # For Devise
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 end
