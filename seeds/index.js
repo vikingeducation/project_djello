65ce2855b7237f678cec22272482adfb5cfeb6b9
Will Timpson <will.timpson@gmail.com>
2017-09-17 17:37:39 -0400

    Seeds!

--- seeds/index.js
+++ seeds/index.js
@@ -1,57 +1,43 @@
 const models = require("../models");
 const faker = require("faker");
 const voca = require("voca");
-const mongoseeder = require("mongooseeder");
+const mongooseeder = require("mongooseeder");
 
-let { Comment, User, Post } = models;
+let { User, Board } = models;
 
 function seeds() {
   let users = [];
-  for (let i = 0; i < 10; i++) {
+  for (let i = 0; i < 5; i++) {
     let user = new User({
       username: faker.random
         .words(2)
         .split(" ")
         .join(""),
-      email: faker.internet.email()
+      password: "pass"
     });
     users.push(user);
   }
 
-  let posts = [];
+  let boards = [];
   for (let i = 0; i < 20; i++) {
-    let post = new Post({
-      title: voca.titleCase(faker.random.words(6)),
-      body: faker.random.words(100),
-      user: users[i % 10],
-      score: faker.random.number({ min: -100, max: 100 })
+    let board = new Board({
+      title: voca.titleCase(faker.random.words(4)),
+      user: users[i % 10]
     });
-    users[i % 10].posts.push(post);
-    posts.push(post);
+    users[i % 5].boards.push(board);
+    board.members.push(users[i % 5]);
+    boards.push(board);
   }
 
-  let comments = [];
-  for (let i = 0; i < 200; i++) {
-    let comment = new Comment({
-      body: faker.random.words(50),
-      user: users[i % 10],
-      post: posts[i % 20]
-    });
-    users[i % 10].comments.push(comment);
-    posts[i % 20].comments.push(comment);
-    comments.push(comment);
-  }
+  const promises = [users, boards].reduce(
+    (ps, collection) => ps.concat(collection.map(model => model.save())),
+    []
+  );
 
-  let promises = [];
-  [posts, users, comments].forEach(collection => {
-    collection.forEach(model => {
-      promises.push(model.save());
-    });
-  });
   return Promise.all(promises);
 }
 
-mongoseeder.seed({
+mongooseeder.seed({
   mongodbUrl: require("../config/mongoUrl"),
   models: models,
   clean: true,
