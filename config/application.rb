099a8d6118e31fbb783ed9c45a380173416dba9d
Yi-Xuan Lau <yixuan.lau@gmail.com>
2017-10-22 20:37:47 +0800

    Enable CORS

--- config/application.rb
+++ config/application.rb
@@ -26,5 +26,12 @@ module Djello
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins 'localhost:4000'
+        resource '*', :headers => :any, :methods => [:get, :post]
+      end
+    end
   end
 end
