a684c1b257861db9cb60e47985e01e9dd08aaab2
Yi-Xuan Lau <yixuan.lau@gmail.com>
2017-11-19 00:44:47 +0800

    Ensure all boards for a user are returned

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -2,7 +2,7 @@ class MainController < ApplicationController
   respond_to :json
 
   def index
-    if current_user.boards.empty?
+    if current_user.all_boards.empty?
       return head :not_found
     else
       @user = User.includes(board_memberships: {board: {lists: [:cards]}}).where(id: current_user.id).first
