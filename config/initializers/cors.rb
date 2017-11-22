7091439611964f3328c3e25912cb835f44b79940
Yi-Xuan Lau <yixuan.lau@gmail.com>
2017-11-22 23:08:17 +0800

    Use ENV vars for CORS origin

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,8 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    # origins 'https://yxlau.github.io'
-    origins 'http://localhost:4000'
+    origins ENV['CORS_ORIGINS']
     resource '*',
       headers: :any,
       :methods => [:get, :post, :put, :delete]
