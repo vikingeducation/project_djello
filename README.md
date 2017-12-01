9a8cdb76993050f00f3361d44868b7f58571eb3d
Chuck Michael <chuckqs@yahoo.com>
2017-12-01 12:31:53 -0500

    Updated readme, updated server for production calls

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # assignment_djello
+
 Project management with that great wobbly taste.
+
+# Live demo:
+
+* https://chuckstaskboards.herokuapp.com/
+
+# Production reset:
+
+* heroku run sequelize db:migrate:undo:all && heroku run sequelize db:migrate &&
+  heroku run sequelize db:seed:all
+
+# For Production:
+
+* change fetches urls to server's location
+
+# development setup:
+
+* npm install
+
+* add .env with SECRET key if desired change
+* config/config.json to database name
+* in root dir run npm start
+* --or--
+* in root dir run
+* npm run server in /client run npm start
--- server.js
+++ server.js
@@ -25,6 +25,24 @@ if (process.env.NODE_ENV !== "production") {
   require("dotenv").config();
 }
 
+var allowCrossDomain = function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Content-Type, Authorization, Content-Length, X-Requested-With"
+  );
+
+  // intercept OPTIONS method
+  if ("OPTIONS" == req.method) {
+    res.send(200);
+  } else {
+    next();
+  }
+};
+
+app.use(allowCrossDomain);
+
 // ----------------------------------------
 // Body Parser
 // ----------------------------------------
